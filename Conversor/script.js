85d04003876a06e827e92e804a56f1b822858c22
PedroHenriqueDosSantosNL <pedrimsantos1@hotmail.com>
2025-10-28 19:51:33 -0300

    adiciona logo animado e aprimora a estrutura do HTML e CSS do conversor de moedas

--- Conversor/script.js
+++ Conversor/script.js
@@ -1,7 +1,24 @@
-function cliqueiNoBotao() {
-    const valorInput = document.getElementById('valorParaConversao');
-    const valor = parseFloat(valorInput.value.replace(',', '.'));
+const convertButton = document.querySelector('.convert-button')
 
-    document.getElementById('valorVariavelInicial').textContent = valor.toFixed(2);
-    document.getElementById('valorVariavelFinal').textContent = (valor / 5.43).toFixed(2);
+function convertValues(){
+    const inputCurrencyValue = document.querySelector('.input-currency').value
+    const currencyValueToConvert = document.querySelector('.currency-value-to-convert')
+    const currencyValueConverted = document.querySelector('.currency-value')
+
+    const dolarToday = 5.48
+
+    const convertedValue = inputCurrencyValue / dolarToday
+
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+    }).format(inputCurrencyValue)
+
+    currencyValueConverted.innerHTML = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD'
+    }).format(convertedValue)
+
 }
+
+convertButton.addEventListener('click', convertValues)
